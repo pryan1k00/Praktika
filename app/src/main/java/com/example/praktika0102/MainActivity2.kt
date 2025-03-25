23bd6cd0165b8850db46531cbdc4d85792838003
pryan1k00 <tulskiyivan06@gmail.com>
2025-03-25 11:53:48 +0300

    Zadanie3

--- app/src/main/java/com/example/praktika0102/MainActivity2.kt
+++ app/src/main/java/com/example/praktika0102/MainActivity2.kt
@@ -5,20 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
-        val btVal = findViewById<Button>(R.id.btDal)
-        btVal.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-        }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
